ea2c1eb046e555b719eefd25225e0a3372c35b5d
AnonimXSs <103380378+AnonimXSs@users.noreply.github.com>
2022-05-03 18:32:00 +0400

    Update config.py

--- config.py
+++ config.py
@@ -11,13 +11,21 @@ bot_token = os.environ.get("TOKEN")
 # Telethon 
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 #
-USERNAME = os.environ.get("USERNAME")
+USERNAME = os.environ.get("USERNAME")DiamonTaggerBot
 log_qrup = int(os.environ.get("LOG_QRUP"))
-startmesaj = os.environ.get("startmesaj")
-komutlar = os.environ.get("komutlar")
-qrupstart = os.environ.get("qrupstart")
-support = os.environ.get("support")
-sahib = os.environ.get("sahib")
+startmesaj = os.environ.get("startmesaj")Salam** __Çox funksiyalı tağ botu
+❗Qrupda Admin Etmək Vacibdir ★__
+komutlar = os.environ.get("komutlar")Kömək Menyusuna Xoş gəldin 🗽
+/all <səbəb> - Hər kəsi mesajda 5 tağ olmaqla tağ edər
+/admin <səbəb> - Adminləri mesajda 1 tağ olmaqla tağ edər
+/tektag <səbəb> - Hər kəsi mesajda 1 tağ olmaqla tağ edər
+/etag  <səbəb> - Hər kəsi mesajda 5 emoji olmaqla emojilərlə tağ edər
+/rtag  <səbəb> - İstifadəçiləri rənglərlə etiketləyirəm
+/cancel - Tağ Prosesini Sonlandırar
+❕ Bu əmrlərdən yalnız idarəçilər istifadə edə bilər. ✨
+qrupstart = os.environ.get("qrupstart")✅ **Mən Aktivəm !** 🕊️ **Əmrləri görmək üçün pm-ə baxa bilərsiniz.*
+support = os.environ.get("support") https://t.me/DiamondTaggerSupport
+sahib = os.environ.get("sahib") https://t.me//TheMrMuazzam
 #
 # mutsuz_panda 
 # mutsuz_panda 
