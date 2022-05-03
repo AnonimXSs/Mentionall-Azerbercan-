59071b34b25a0eeb0041ac67bdb4c2e62efa6d98
AnonimXSs <103380378+AnonimXSs@users.noreply.github.com>
2022-05-03 18:21:59 +0400

    Update config.py

--- config.py
+++ config.py
@@ -17,8 +17,6 @@ startmesaj = os.environ.get("startmesaj")
 komutlar = os.environ.get("komutlar")
 qrupstart = os.environ.get("qrupstart")
 support = os.environ.get("support")
-sahib = os.environ.get("sahib")
+sahib = os.environ.get("sahib") f'https://t.me/TheMrMuazzam
 #
-# mutsuz_panda 
-# mutsuz_panda 
-# mutsuz_panda 
+
